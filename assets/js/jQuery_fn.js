57d26d8117687760814ff8c9281ed0bf087c1112
rafa-rivadeneyra <rafarivadeneyra@gmail.com>
2023-09-15 15:29:54 -0600

    Se separa el código de menú sticky

--- assets/js/jQuery_fn.js
+++ assets/js/jQuery_fn.js
@@ -24,3 +24,43 @@ $(document).ready(function() {
     );
 });
 
+/************   Menú sticky   ***********/
+
+$(document).ready(function() {
+    var menu = $("#menu");
+    var menuOffset = menu.offset().top; // Obtener la posición superior del menú original
+
+    // Manejar el evento de scroll
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop(); // Obtener la posición de desplazamiento actual
+
+        // Verificar si el usuario ha desplazado más allá de la posición original del menú
+        if (scrollPos >= menuOffset) {
+            menu.addClass("fixed-menu"); // Aplicar clase para posición fija
+        } else {
+            menu.removeClass("fixed-menu"); // Quitar clase de posición fija
+        }
+    });
+});
+
+
+$(document).ready(function() {
+    // Mostrar el botón de scroll cuando se desplaza hacia abajo 100 píxeles
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() > 100) {
+            var scrollTop_1 = window.scrollY || document.documentElement.scrollTop;
+            scrollTop_1 = scrollTop_1 - 100;
+            console.log(window.scrollY, scrollTop_1);
+        } else {
+            
+        }
+    });
+
+    // Hacer scroll hacia arriba al hacer clic en el botón
+    $('.scrollMenu').click(function() {
+        $('.scrollMenu').css('margin-top', '+8px');
+    });
+});
+
+/************  Fin menú sticky   ***********/
